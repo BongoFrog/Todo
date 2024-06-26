1625f5f349d419d5b6f183b6d49607e2d932e833
BongoFrog <canhlong2012@mail.com>
2024-06-26 10:12:21 +0700

    Added to repo

--- server/index.js
+++ server/index.js
@@ -2,8 +2,17 @@
 require('dotenv').config(); // Load environment variables
 const taskRoutes = require('./task/taskRoutes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const express = require('express');
 const app = express();
+
+app.use(cors());
+app.use(cors({
+  origin: [process.env.FRONTEND_URL], // Allow these origins
+  methods: ['GET', 'POST', 'PUT','DELETE'],                             // Allow these methods
+  allowedHeaders: ['Content-Type', 'Authorization'],           // Allow these headers
+  credentials: true                                           // Allow credentials (cookies, authorization headers)
+}));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
